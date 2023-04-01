626db4c76bf4e6c06834f2f51a5ec9db25cf9867
Claire Kreisel <claire@clairekreisel.com>
2023-04-01 17:49:31 -0400

    added command byte preprocessor vars

--- arduino/scd_rad_logger/scd_memToSerial.ino
+++ arduino/scd_rad_logger/scd_memToSerial.ino
@@ -33,14 +33,24 @@ PIN SETUP FOR ELECTRICAL PEOPLE
     #define MEM_SIO3 31
 #endif
 
-#define MEM_BITC 12
-#define MEM_CHANNEL_BYTE 0//0 = channel 1, 1 = channel 2
-#define MEM_CHB_LOC 3
+#define MEM_EQIO 0x35
+#define MEM_EN4B 0xB7
+#define MEM_SUS 0xB0 //suspends current program/erase
+#define MEM_RES 0x30 //resumes suspended program/erase
+#define MEM_DP 0xB9 //deep power down
+#define MEM_RDP 0xAB //release deep power down
+#define MEM_WREN 0x06 //write enable
+#define MEM_WRDI 0x04 //write disable
+#define MEM_SE 0x20 //sector erase
+#define MEM_READ 0xEC //4READ4B on datasheet, next 4 bytes are memory address, 6th is dummy
+#define MEM_WRITE 0x3E //4PP4B on datasheet, next 4 bytes are memory addredd, followed by 1-256 data cycles
 
 void setup() {
 
-    pinMode(MEM_DIN, OUTPUT);
-    pinMode(MEM_DOUT, INPUT);
+    pinMode(MEM_SIO0, OUTPUT);
+    pinMode(MEM_SIO1, OUTPUT);
+    pinMode(MEM_SIO2, OUTPUT);
+    pinMode(MEM_SIO3, OUTPUT);
     pinMode(MEM_SCLK, OUTPUT);
     pinMode(MEM_CS, OUTPUT);
 

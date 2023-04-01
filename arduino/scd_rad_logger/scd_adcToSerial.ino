c3fd65edd8b3297cac5e034e093465c0428efc67
Claire Kreisel <claire@clairekreisel.com>
2023-04-01 16:39:58 -0400

    fixed whitespace on adc

--- arduino/scd_rad_logger/scd_adcToSerial.ino
+++ arduino/scd_rad_logger/scd_adcToSerial.ino
@@ -1,22 +1,23 @@
 #ifndef SPEXSAT_BAUD
-#define SPEXSAT_BAUS 9600
+    #define SPEXSAT_BAUS 9600
 #endif
+
 #include <SPI.h>
 
 #define SPEXSAT_BOARD_MEGA
 
 #ifdef SPEXSAT_BOARD_UNO
-#define ADC_DOUT 11 //This connects to the DOUT pin on the ADC
-#define ADC_DIN 12
-#define ADC_SCLK 13
-#define ADC_CS 10
+    #define ADC_DOUT 11 //This connects to the DOUT pin on the ADC
+    #define ADC_DIN 12
+    #define ADC_SCLK 13
+    #define ADC_CS 10
 #endif
 
 #ifdef SPEXSAT_BOARD_MEGA
-#define ADC_DOUT 23 //This connects to the DOUT pin on the ADC
-#define ADC_DIN 24
-#define ADC_SCLK 25
-#define ADC_CS 22
+    #define ADC_DOUT 23 //This connects to the DOUT pin on the ADC
+    #define ADC_DIN 24
+    #define ADC_SCLK 25
+    #define ADC_CS 22
 #endif
 
 #define ADC_BITC 12
@@ -25,52 +26,52 @@
 
 void setup() {
 
-pinMode(ADC_DIN, OUTPUT);
-pinMode(ADC_DOUT, INPUT);
-pinMode(ADC_SCLK, OUTPUT);
-pinMode(ADC_CS, OUTPUT);
+    pinMode(ADC_DIN, OUTPUT);
+    pinMode(ADC_DOUT, INPUT);
+    pinMode(ADC_SCLK, OUTPUT);
+    pinMode(ADC_CS, OUTPUT);
 
-digitalWrite(ADC_CS, HIGH);
-digitalWrite(ADC_IN, LOW);
-digitalWrite(ADC_SCLK, LOW);
+    digitalWrite(ADC_CS, HIGH);
+    digitalWrite(ADC_IN, LOW);
+    digitalWrite(ADC_SCLK, LOW);
 
-Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 int read_adc(){
-long adcVal=0;
-byte cmdBits=b00000000 | ADC_CHANNEL_BYTE << ADC_CHB_LOC;
+    long adcVal=0;
+    byte cmdBits=b00000000 | ADC_CHANNEL_BYTE << ADC_CHB_LOC;
 
-digitalWrite(ADC_CS, LOW);
+    digitalWrite(ADC_CS, LOW);
 
-//writes command bits
-for(int i = 7; i>=4; i--){
-digitalWrite(ADC_DIN, cmdBits & 1<<i);
-digitalWrite(ADC_SCLK, HIGH);
-digitalWrite(ADC_SCLK, LOW);
-}
-int j = ADC_BITC-1
-for(int i = 3; i>= 0; i--, j--){
-digitalWrite(ADC_DIN, cmdBits & 1<<i);
-adcVal += digitalRead(ADC_DOUT)<<j;
-digitalWrite(ADC_SCLK, HIGH);
-digitalWrite(ADC_SCLK, LOW);
-}
+    //writes command bits
+    for(int i = 7; i>=4; i--){
+        digitalWrite(ADC_DIN, cmdBits & 1<<i);
+        digitalWrite(ADC_SCLK, HIGH);
+        digitalWrite(ADC_SCLK, LOW);
+    }
+    int j = ADC_BITC-1
+    for(int i = 3; i>= 0; i--, j--){
+        digitalWrite(ADC_DIN, cmdBits & 1<<i);
+        adcVal += digitalRead(ADC_DOUT)<<j;
+        digitalWrite(ADC_SCLK, HIGH);
+        digitalWrite(ADC_SCLK, LOW);
+    }
 
 
-for(; j>=0; j--){
-adcVal += digitalRead(ADC_DOUT)<<j;
-digitalWrite(ADC_SCLK, HIGH);
-digitalWrite(ADC_SCLK, LOW);
-}
-digitalWrite(ADC_CS, HIGH);
+    for(; j>=0; j--){
+        adcVal += digitalRead(ADC_DOUT)<<j;
+        digitalWrite(ADC_SCLK, HIGH);
+        digitalWrite(ADC_SCLK, LOW);
+    }
+    digitalWrite(ADC_CS, HIGH);
 
-return adcVal;
+    return adcVal;
 }
 
 void loop() {
 
-Serial.println("Val: "+read_adc() + " Time: " + millis());
+    Serial.println("Val: "+read_adc() + " Time: " + millis());
 
 }
 

38eeb23e01733841d5ce9ce6c72c17f2df399ba2
Claire Kreisel <claire@clairekreisel.com>
2023-09-17 18:26:13 -0400

    Basic SD implementation

---
+++ arduino/scd_rad_logger/scd_memToSerial_SD/scd_memToSerial_SD.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+#include <SD.h>
+
+#ifndef SPEXSAT_BAUD
+#define SPEXSAT_BAUD 9600
+#endif
+
+#ifndef SPEXSAT_BOARD_MEGA
+#define SPEXSAT_BOARD_MEGA 0
+#endif
+
+#ifndef SPEXSAT_BOARD_UNO
+#define SPEXSAT_BOARD_UNO 1
+#endif
+
+#ifndef SPEXSAT_BOARD
+#define SPEXSAT_BOARD SPEXSAT_BOARD_MEGA
+#endif
+
+#if SPEXSAT_BOARD == SPEXSAT_BOARD_MEGA
+#define SD_CS_PIN 53
+#else
+#define SD_CS_PIN
+#endif
+
+File dataCSV;
+void setup_SD(){
+    while(!SD.begin(SD_CS_PIN));
+}
+
+void write_SD_line(void* data_arr, unsigned int array_len) {
+    dataCSV = SD.open("data.csv", FILE_WRITE);
+    for(int i = 0; i < array_len; i++){
+        dataCSV.print(*(data_arr+i));
+        if(i != array_len -1){
+            dataCSV.print(',');
+    }
+    dataCSV.println();
+    dataCSV.close();
+}
